2436a696c82c529560a4dfee59f00604c4661b2d
vinhnq <vinhnq@zigexn.vn>
2018-06-21 10:01:21 +0700

    fix some rubocop error

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,16 +1,14 @@
 # frozen_string_literal: true
 
 class JobsController < ApplicationController
-
   def index
-      search_str = params[:search].blank? ? '*' : params[:search]
-      job_found  = Job.search(kw: search_str, page: params[:page].to_i)
-      job_ids   = job_found[:job_id]
-      @job_num   = job_found[:num]
-      job_search_result = Job.where(id: job_ids)
-      @jobs = Kaminari.paginate_array(job_search_result,
-                                      total_count: @job_num).
-                       page(params[:page])
+    search_str = params[:search].blank? ? '*' : params[:search]
+    job_found = Job.search(kw: search_str, page: params[:page].to_i)
+    job_ids = job_found[:job_id]
+    @job_num = job_found[:num]
+    job_search_result = Job.where(id: job_ids)
+    @jobs = Kaminari.paginate_array(job_search_result, total_count: @job_num)
+                    .page(params[:page])
   end
 
   def show
